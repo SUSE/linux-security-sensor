15f51cdf8066f74227316e2739da04e1dc470cf5
Jeff Mahoney <jeffm@suse.com>
2023-02-05 05:57:27 -0500

    vql/functions/hash: cache results on Linux
    When we use Velociraptor on systems with a big fork/exec workload,
    use system call auditing, and hash the executable, we end up with
    pinned CPUs for obvious reasons.  There's no value in hashing the same
    file over and over if we know it hasn't changed.
    
    This commit adds an LRU cache for the hash results.  It uses the
    dev number, inode number, mtime, and ctime to ensure that the file
    hasn't been modified.  A race does exist betewen stat and open, but
    for this use case a bigger race exists between exec and the hash
    being requested.

---
+++ vql/functions/hash_cache_windows.go
@@ -0,0 +1,28 @@
+package functions
+
+import (
+	"errors"
+)
+
+type uncachedHashResultEntry struct {}
+
+var(
+	cacheSize int64 = 0
+	errNotImplemented = errors.New("Hash caching is not implemented for Windows")
+)
+
+func (self *uncachedHashResultEntry) Size() int {
+	return 1
+}
+
+func (self *uncachedHashResultEntry) Validate(filename string) (bool, error) {
+	return false, errNotImplemented
+}
+
+func (self *uncachedHashResultEntry) Result() *HashResult {
+	return nil
+}
+
+func newHashResultCacheEntry(filename string) (*uncachedHashResultEntry, error) {
+	return nil, errNotImplemented
+}
