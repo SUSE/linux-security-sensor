b42d5564aa777aa960a2b31dcae1c86fa6443e6c
Jeff Mahoney <jeffm@jeffm.io>
2023-04-09 10:03:42 -0400

    vql: move upload plugins to vql/upload

--- vql_plugins/plugins.go
+++ vql_plugins/plugins.go
@@ -41,4 +41,6 @@ import (
 	_ "www.velocidex.com/golang/velociraptor/vql/tools"
 	_ "www.velocidex.com/golang/velociraptor/vql/tools/collector"
 	_ "www.velocidex.com/golang/velociraptor/vql/tools/process"
+	_ "www.velocidex.com/golang/velociraptor/vql/upload"
+	_ "www.velocidex.com/golang/velociraptor/vql/upload/humio"
 )
