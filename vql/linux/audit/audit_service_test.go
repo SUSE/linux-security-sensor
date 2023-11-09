a54c21641a15075b7d6eabfb788c7957d06c4e3d
Jeff Mahoney <jeffm@suse.com>
2023-11-09 11:18:44 -0500

    vql/linux/audit: handle top-level cancelation properly
    We don't need to attach the cancelation function to the service.  We can
    use a channel instead and close it when all subscribers have
    unsubscribe.

--- vql/linux/audit/audit_service_test.go
+++ vql/linux/audit/audit_service_test.go
@@ -157,7 +157,7 @@ func (self *AuditServiceTestSuite) TestRunService() {
 
 	self.auditService.serviceLock.Lock()
 	self.auditService.shuttingDown = true
-	self.auditService.cancelService()
+	close(self.auditService.subscriberChan)
 	self.auditService.serviceLock.Unlock()
 }
 
