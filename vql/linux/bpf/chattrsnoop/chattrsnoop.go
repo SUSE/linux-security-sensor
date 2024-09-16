c227b64e223059afe4fdaf029a09a706eb3fd99c
Darragh O'Reilly <doreilly@suse.com>
2024-09-16 14:33:31 +0100

    bpf: fix plugins not stopping when context cancelled
    When the client event table changes, Velociraptor cancels each plugin's
    context and expects it to shut itself down. Then it starts them again
    if they are still needed in the new table. The bpf based plugins were
    not stopping and Velociraptor was starting new goroutines for them,
    leaving the old ones running and leaking resources forever.
    
    This commit changes the bpf plugins so they now exit when the context
    is cancelled. Velociraptor does not wait until the plugins are fully
    stopped before starting new instances and the concurrent loading and
    unloading of BPF modules is problematic. To ensure only one BPF module
    load or unload operation is happening at any one time, this commit
    introduces a publish/subscribe service.

--- vql/linux/bpf/chattrsnoop/chattrsnoop.go
+++ vql/linux/bpf/chattrsnoop/chattrsnoop.go
@@ -6,8 +6,10 @@ import (
 	"context"
 	_ "embed"
 	"errors"
+	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/Velocidex/ordereddict"
@@ -18,7 +20,12 @@ import (
 	"www.velocidex.com/golang/velociraptor/logging"
 	"www.velocidex.com/golang/velociraptor/vql"
 	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	"www.velocidex.com/golang/velociraptor/vql/linux/bpf"
 	"www.velocidex.com/golang/vfilter"
+)
+
+const (
+	CHATTRSNOOP = "chattrsnoop"
 )
 
 type ChattrsnoopPlugin struct{}
@@ -70,6 +77,8 @@ func (self ChattrsnoopPlugin) Call(
 	output_chan := make(chan vfilter.Row)
 
 	go func() {
+		defer close(output_chan)
+
 		err := vql_subsystem.CheckAccess(scope, acls.MACHINE_STATE)
 		if err != nil {
 			scope.Log("chattrsnoop: %s", err)
@@ -84,12 +93,49 @@ func (self ChattrsnoopPlugin) Call(
 		config_obj := &config_proto.Config{Client: client_config_obj}
 		logger := logging.GetLogger(config_obj, &logging.ClientComponent)
 
-		bpfModule, err := initBpf(logger)
+		subscriber := bpf.GetManager().Subscribe(CHATTRSNOOP, &publisher{logger: logger})
+		defer bpf.GetManager().Unsubscribe(CHATTRSNOOP, subscriber)
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case event := <-subscriber.EventCh:
+				output_chan <- event
+
+			case err := <-subscriber.ErrorCh:
+				scope.Log("%v", err)
+				return
+			}
+		}
+	}()
+
+	return output_chan
+}
+
+type publisher struct {
+	wg     sync.WaitGroup
+	cancel func()
+	logger *logging.LogContext
+}
+
+func (p *publisher) Start() {
+	var ctx context.Context
+	ctx, p.cancel = context.WithCancel(context.Background())
+	bpfModuleLoadDoneCh := make(chan struct{})
+
+	p.wg.Add(1)
+	go func() {
+		defer p.wg.Done()
+
+		bpfModule, err := initBpf(p.logger)
+		bpfModuleLoadDoneCh <- struct{}{}
 		if err != nil {
-			scope.Log("chattrsnoop: Error initialising bpf: %s", err)
+			e := fmt.Errorf("chattrsnoop: Error initialising bpf: %s", err)
+			bpf.GetManager().PublishError(ctx, CHATTRSNOOP, e)
 			return
 		}
-
 		defer bpfModule.Close()
 
 		eventsChan := make(chan []byte)
@@ -97,22 +143,41 @@ func (self ChattrsnoopPlugin) Call(
 
 		perfBuffer, err := bpfModule.InitPerfBuf("events", eventsChan, lostChan, 128)
 		if err != nil {
-			scope.Log("chattrsnoop: Error opening bpf communication channel: %s", err)
+			e := fmt.Errorf("chattrsnoop: Error opening bpf communication channel: %s", err)
+			bpf.GetManager().PublishError(ctx, CHATTRSNOOP, e)
 			return
 		}
 
 		perfBuffer.Poll(300)
 
-		for data := range eventsChan {
-			event, err := parseData(data)
-			if err != nil {
-				scope.Log("chattrsnoop: parsing event: %v", err)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case data, ok := <-eventsChan:
+				if !ok {
+					e := fmt.Errorf("chattrsnoop: event channel was closed")
+					bpf.GetManager().PublishError(ctx, CHATTRSNOOP, e)
+					return
+				}
+
+				event, err := parseData(data)
+				if err != nil {
+					p.logger.Warnf("chattrsnoop: error parsing event: %v", err)
+					continue
+				}
+				bpf.GetManager().PublishEvent(ctx, CHATTRSNOOP, event)
 			}
-			output_chan <- event
 		}
 	}()
 
-	return output_chan
+	<-bpfModuleLoadDoneCh // wait until the BPF module is loaded
+}
+
+func (p *publisher) Stop() {
+	p.cancel()
+	p.wg.Wait()
 }
 
 func init() {
